245fe5d26dfe20615a0048d6f35258de5191bc04
deindedawodu <mr.dawodu@gmail.com>
2016-01-21 17:06:12 -0500

    editing and save functionality added

--- js/Note.js
+++ js/Note.js
@@ -1,21 +1,45 @@
 var Note = React.createClass({
+	       getInitialState:function(){
+         return {editing:false}
+	      },
 	      edit: function(){
-	      	alert('editing note');
+	      	this.setState({editing:true});
+	      },
+	      save: function(){
+	      	 this.setState({editing:false});
 	      },
 	      remove: function(){
 	      	alert('removing note');
 	      },
-        render: function(){
-            return(
+	      renderDisplay: function(){
+	      	    return(
                     <div className="note">
                     <p>{this.props.children}</p>
                       <span>
                         <button onClick={this.edit} className="btn btn-primary glyphicon glyphicon-pencil"/>
-                        <button onClick={this.remove} className="btn btn-danger glyphicon glyphicon-pencil"/>
+                        <button onClick={this.remove} className="btn btn-danger glyphicon glyphicon-trash"/>
                       </span>
                     </div>
 
-            	) 
+            	);
+
+	      },
+	      renderForm: function(){
+	      	return(
+             <div className="note">
+               <textarea defaultValue={this.props.children} className="form-control"></textarea>
+               <button onClick={this.save} className="btn btn-success btn-sm glyphicon glyphicon-floppy-disk">Submit</button>
+             </div>
+
+	      		);
+
+	      },
+        render: function(){
+           if(this.state.editing){
+           	return this.renderForm();
+           }else{
+           	return this.renderDisplay();
+           }
                   
         }
      }); 
